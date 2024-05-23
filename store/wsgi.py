a677a8c6b873f478b3900ef86c8a8b44665d942f
gordi <gordiy.krizanovskiy@knu.ua>
2024-05-23 23:47:31 +0300

    ...

--- store/wsgi.py
+++ store/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for store project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+from accounts.views import create_manager
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
+
+# create user with 'manager' role
+create_manager()
 
 application = get_wsgi_application()

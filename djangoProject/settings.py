786eacc9fa16a19597e7c150a6505f2b700155ae
gordi <gordiy.krizanovskiy@knu.ua>
2024-05-27 18:36:39 +0300

    initial

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('*')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ALLOWED_HOSTS
+ALLOWED_HOSTS = [ALLOWED_HOSTS]
 
 
 # Application definition

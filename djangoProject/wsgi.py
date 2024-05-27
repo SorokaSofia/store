4e5807bb28fc9605516b48d67d70381878b74806
Alexei Gopkalo <vn.gopkalo1978@gmail.com>
2024-05-27 22:43:01 +0300

    ok

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -1,12 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-from accounts.views import create_manager
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
-
-# create user with 'manager' role
-create_manager()
 
 application = get_wsgi_application()
